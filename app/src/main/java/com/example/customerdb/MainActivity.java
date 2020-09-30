f30ebab2dc5ae2f08091fc184059be1afb40b216
Arskaman <a.laihorinne@gmail.com>
2020-09-30 17:02:56 +0300

    Napit ja clicListenerit mainiin

--- app/src/main/java/com/example/customerdb/MainActivity.java
+++ app/src/main/java/com/example/customerdb/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.customerdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv_customerList;
+    FloatingActionButton add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv_customerList = findViewById(R.id.lv_customerList);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/customerdb/MyDatabaseHelper.java
+++ app/src/main/java/com/example/customerdb/MyDatabaseHelper.java
@@ -9,16 +9,16 @@ import androidx.annotation.Nullable;
 class MyDatabaseHelper extends SQLiteOpenHelper {
 
     private Context context;
-    public static final String DATABASE_NAME = "CustomerDB.db";
-    public static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "CustomerDB.db";
+    private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_NAME = "my_customers";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_NAME = "customer_name";
-    public static final String COLUMN_COMPANY = "customer_company";
-    public static final String COLUMN_CITY = "customer_city";
-    public static final String COLUMN_PHONE = "customer_phone"; // INTEGER!
-    public static final String COLUMN_EMAIL = "customer_email";
+    private static final String TABLE_NAME = "my_customers";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NAME = "customer_name";
+    private static final String COLUMN_COMPANY = "customer_company";
+    private static final String COLUMN_CITY = "customer_city";
+    private static final String COLUMN_PHONE = "customer_phone"; // INTEGER!
+    private static final String COLUMN_EMAIL = "customer_email";
 
     public MyDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -40,7 +40,9 @@ class MyDatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
 
     }
 }
