38b81bb204b42eb9d6e08edbdd620c00eaaf3cdb
Arskaman <a.laihorinne@gmail.com>
2020-09-30 16:51:09 +0300

    onCreate

--- app/src/main/java/com/example/customerdb/MyDatabaseHelper.java
+++ app/src/main/java/com/example/customerdb/MyDatabaseHelper.java
@@ -17,15 +17,25 @@ class MyDatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_NAME = "customer_name";
     public static final String COLUMN_COMPANY = "customer_company";
     public static final String COLUMN_CITY = "customer_city";
-    public static final String COLUMN_PHONE = "customer_phone";
+    public static final String COLUMN_PHONE = "customer_phone"; // INTEGER!
     public static final String COLUMN_EMAIL = "customer_email";
 
     public MyDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        String guery =
+                "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_NAME + " TEXT, " +
+                        COLUMN_COMPANY + " TEXT, " +
+                        COLUMN_CITY + " TEXT, " +
+                        COLUMN_PHONE + " INTEGER, " +
+                        COLUMN_EMAIL + " TEXT);";
+        db.execSQL(guery);
 
     }
 
