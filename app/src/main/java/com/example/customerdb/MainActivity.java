9a3a844cd57fcfcc3c68b634b594bc85df3cbe2f
Arskaman <a.laihorinne@gmail.com>
2020-10-03 13:23:55 +0300

    My_row

--- app/src/main/java/com/example/customerdb/MainActivity.java
+++ app/src/main/java/com/example/customerdb/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.customerdb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    ListView lv_customerList;
+    RecyclerView recyclerView;
     FloatingActionButton add_button;
+    MyDatabaseHelper myDB;
+    ArrayList<String> _id, customer_name, customer_company, customer_city, customer_phone, customer_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv_customerList = findViewById(R.id.lv_customerList);
+        recyclerView = findViewById(R.id.recyclerView);
         add_button = findViewById(R.id.add_button);
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +37,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        myDB = new MyDatabaseHelper(MainActivity.this);
+        _id = new ArrayList<>();
+        customer_name = new ArrayList<>();
+        customer_company = new ArrayList<>();
+        customer_city = new ArrayList<>();
+        customer_phone = new ArrayList<>();
+        customer_email = new ArrayList<>();
+
+
+        storeDataInArrays();
+
+    }
+
+    void storeDataInArrays () {
+        Cursor cursor = myDB.readAllData();
+        if (cursor.getCount() == 0) {
+            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                _id.add(cursor.getString(0));
+                customer_name.add(cursor.getString(1));
+                customer_company.add(cursor.getString(2));
+                customer_city.add(cursor.getString(3));
+                customer_phone.add(cursor.getString(4));
+                customer_email.add(cursor.getString(5));
+            }
+        }
     }
 }
